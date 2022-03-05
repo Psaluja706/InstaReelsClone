e7eb1d14e1afe1aebabeadf60609cf2b16263db7
Priyanshu Saluja <priyanshu1231.cse19@chitkara.edu.in>
2022-03-05 18:45:27 +0530

    New Commit

---
+++ src/components/secrets.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyDnVNCrMcpYdrjJkz0jpyDm2ldVPKzEAC0",
+  authDomain: "socialmedia-9f947.firebaseapp.com",
+  projectId: "socialmedia-9f947",
+  storageBucket: "socialmedia-9f947.appspot.com",
+  messagingSenderId: "2778661022",
+  appId: "1:2778661022:web:7351c8df41fd3b99177b8f"
+};
